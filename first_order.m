8cd016a9772362bae86c3d2742ba9843a9c014ae
Tomás Fontes <55455167+baetatomska98@users.noreply.github.com>
2020-02-22 20:38:52 +0100

    Add files via upload

---
+++ first_order.m
@@ -0,0 +1,5 @@
+function ddeltach4=first_order(year,delta_ch4,ch4_tchange,ch4_ppbv,tau)
+    a=((ch4_tchange./(1+ch4_tchange))./tau).*ch4_ppbv;
+    b=(1./(1+ch4_tchange))./tau;
+    ddeltach4=(a-b.*delta_ch4);
+end
